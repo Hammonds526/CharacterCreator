effb62fa0389bfc88efd392964b74f2483a5a66f
C.T <ct.bell@yahoo.com>
2021-04-10 14:50:05 -0400

    Added Race buttons to CharInfoBox

--- client/src/components/CharInfoBox/index.js
+++ client/src/components/CharInfoBox/index.js
@@ -6,6 +6,33 @@ import WoodBeamX from "../WoodBeamX";
 import WoodBeamY from "../WoodBeamY";
 
 function CharInfoBox(props) {
+  const CharacterInfo = {
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
+neque id risus condimentum congue. Nam vitae condimentum libero, sed
+pulvinar odio. Sed odio diam, rutrum vitae nibh ac, pellentesque commodo
+enim. Cras nisl metus, iaculis eu urna eu, imperdiet porttitor nulla.
+Mauris eleifend erat sed dolor eleifend tincidunt. Integer vitae magna
+augue. Vestibulum diam nunc, eleifend eget nulla quis, mollis congue
+libero. Donec ligula ipsum, ornare quis metus sit amet, bibendum blandit
+leo. Quisque eu magna et mauris malesuada fringilla. Etiam sed faucibus
+mauris. Praesent rhoncus risus at leo congue, in auctor quam molestie.
+Cras eget magna magna. Donec blandit bibendum risus, non consectetur
+
+`,
+    race: [
+      // "Dragonborn",
+      "Dwarf",
+      // "Gnome",
+      "Elf",
+      "Half-Elf",
+      "Halfling",
+      "Half-Orc",
+      "Human",
+
+      // "Tiefling",
+    ],
+  };
+
   return (
     <div class="char-info-box__container p-5 mt-2 text-white">
       <WoodBeamX beamStyle={{ top: "-48px" }} />
@@ -32,39 +59,17 @@ function CharInfoBox(props) {
       />
 
       <h6 className="text-white ml-5 mt-3">Choose a Race</h6>
+      <div className="row">
+        {CharacterInfo.race.map((item) => (
+          <div className="col p-0 d-flex justify-content-center">
+            <Button text={item} />
+          </div>
+        ))}
+      </div>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
-        neque id risus condimentum congue. Nam vitae condimentum libero, sed
-        pulvinar odio. Sed odio diam, rutrum vitae nibh ac, pellentesque commodo
-        enim. Cras nisl metus, iaculis eu urna eu, imperdiet porttitor nulla.
-        Mauris eleifend erat sed dolor eleifend tincidunt. Integer vitae magna
-        augue. Vestibulum diam nunc, eleifend eget nulla quis, mollis congue
-        libero. Donec ligula ipsum, ornare quis metus sit amet, bibendum blandit
-        leo. Quisque eu magna et mauris malesuada fringilla. Etiam sed faucibus
-        mauris. Praesent rhoncus risus at leo congue, in auctor quam molestie.
-        Cras eget magna magna. Donec blandit bibendum risus, non consectetur
-      </p>
-      <p>
-        nibh gravida nec. Donec scelerisque gravida vestibulum. Aliquam
-        dignissim pharetra arcu, eu consectetur purus ultricies in. Pellentesque
-        sed elementum nulla, quis dapibus felis. Nam egestas lorem at gravida
-        efficitur. In hac habitasse platea dictumst. In ac justo sagittis,
-        tempus orci tristique, volutpat sem. Etiam fermentum, nibh et rhoncus
-        porta, magna ex bibendum lacus, sed lacinia erat tortor fermentum urna.
-        Quisque iaculis ultrices libero, in faucibus diam sagittis sit amet.
-      </p>
-      <p>
-        Proin id imperdiet dui. Pellentesque neque dolor, efficitur a massa
-        varius, malesuada commodo odio. Etiam eget neque in sapien sollicitudin
-        egestas at vitae felis. Nam tincidunt nibh varius arcu luctus ultrices.
-        Suspendisse arcu nibh, ultricies at orci ut, cursus condimentum lorem.
-        Donec a ipsum consectetur, cursus nisl ac, bibendum quam. Cras sed orci
-        vel est imperdiet egestas. Vestibulum ante ipsum primis in faucibus orci
-        luctus et ultrices posuere cubilia curae; Quisque aliquet lectus tellus,
-        at consectetur leo mollis dictum. Pellentesque euismod sit amet odio ac
-        sagittis. Duis ut vestibulum felis, nec consectetur dolor.
-      </p>
+      {CharacterInfo.description}
+
+      {/* Nothing below here */}
       <WoodBeamX beamStyle={{ bottom: "-57px" }} />
       <Corner
         cornerStyle={{
