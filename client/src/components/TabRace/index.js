3a01cc61653fff5134f49f21bdb8efa49900ef9f
C.T <ct.bell@yahoo.com>
2021-04-11 22:07:52 -0400

    Added functionality to the 'select' button to move to the next tab

--- client/src/components/TabRace/index.js
+++ client/src/components/TabRace/index.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
 import "./style.css";
 import SelectButton from "../SelectButton";
 import LevelSelector from "../LevelSelecter";
 import Button from "../Button";
 
 function TabRace(props) {
+  let { path, url } = useRouteMatch();
+  const [raceDescription, setRaceDescription] = useState("");
+
+  // Get the dwarf description and put it in state
+  useEffect(() => {
+    setRaceDescription(props.character.race[0].description);
+  }, []);
+
   return (
     <div>
       <h6 className="text-white ml-5 mt-3">Choose a Race</h6>
@@ -19,10 +35,12 @@ function TabRace(props) {
         ))}
       </div>
 
-      {props.character.race[0].description}
+      {raceDescription}
       <div className="d-flex justify-content-between">
         <LevelSelector text={"4"} />
-        <SelectButton utton text={"Select"} />
+        <Link to={"/character-creator/class"}>
+          <SelectButton utton text={"Select"} />
+        </Link>
       </div>
     </div>
   );
