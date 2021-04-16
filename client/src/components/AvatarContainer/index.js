0c4f5b2fd985d1130021a55654e5bac740653f9e
Relten98 <diezhandinoty@gmail.com>
2021-04-16 16:22:50 -0700

    more fixes

--- client/src/components/AvatarContainer/index.js
+++ client/src/components/AvatarContainer/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
 import Avatar from "../../images/man2.png";
-import Frame from "../../images/char_card_frame.png";
-import Vortex from "../../images/char_card_frame.png";
+import Frame from "../../images/char_card_frame3.png";
+
 
 
 function AvatarContainer(props) {
@@ -10,7 +10,6 @@ function AvatarContainer(props) {
     <div>
       <div className="avatar__container text-center">
         <div>
-          <div className="vortex"></div>
           <img
             className="frame"
             src={Frame}
@@ -22,7 +21,7 @@ function AvatarContainer(props) {
             alt="Character Avatar"
           ></img>
         </div>
-        <div id="stonebg"></div>
+        <div className="vortex"></div>
 
       </div>
     </div>
