edb9da2e092fa7227c03604a486e5098dc413f6e
Curtis H <sitrucrettah1@gmail.com>
2021-04-17 20:53:22 -0700

    Add front end API

--- client/src/components/TabFeats/index.js
+++ client/src/components/TabFeats/index.js
@@ -3,8 +3,27 @@ import "./style.css";
 import SelectButton from "../SelectButton";
 import LevelSelector from "../LevelSelecter";
 import Button from "../Button";
+import API from "../../utils/API";
 
 function TabFeats(props) {
+
+  const selectButtonOnClick = () => {
+    // console.log("Button Click");
+    // TO DO: REPLACE THIS HASH WITH AUTHENTICATED USER
+    API.getUser("6078fa52093e11225c2d4b85") 
+      .then(res => 
+        {
+          // SOME DEBUGGING (CHANGING STRINGS TO NUMBERS/ASSIGNING ID'S) BUT WORKS
+          // console.log(res.data.user.characters)
+          res.data.user.characters.push(props.newCharacter)
+          // console.log(res.data.user.characters)
+          // console.log(res.data);
+          API.updateUser("6078fa52093e11225c2d4b85", res.data).then(() => {
+            console.log(res.data.user.characters)
+          })
+        });
+  }
+
   return (
     <div>
       <h4 className="text-white ml-3">Choose your Feats</h4>
@@ -22,7 +41,7 @@ function TabFeats(props) {
       {props.character.race[0].description}
       <div className="d-flex justify-content-between">
         <LevelSelector text={"4"} />
-        <SelectButton utton text={"Select"} />
+        <SelectButton text={"Finish"} selectButtonOnClick={selectButtonOnClick}/>
       </div>
     </div>
   );
---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default {
+  // Gets all users
+  getUsers: function() {
+    return axios.get("/api/users");
+  },
+  // Gets the user with the given id
+  getUser: function(id) {
+    return axios.get("/api/users/" + id);
+  },
+  // Deletes the user with the given id
+  deleteUser: function(id) {
+    return axios.delete("/api/users/" + id);
+  },
+  // Saves a user to the database
+  saveUser: function(userData) {
+    return axios.post("/api/users", userData);
+  },
+  // Update a user
+  updateUser: function(id,userData) {
+    return axios.put("/api/users/" + id, userData);
+  }
+};
