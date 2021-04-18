1d5b8e1f3dc0869e7c2aceab2ee7e280ff75b0cc
unknown <garthbhammonds@gmail.com>
2021-04-18 08:04:43 -0700

    fixed parsing error in spell list

--- client/src/data/spells.js
+++ client/src/data/spells.js
@@ -347,8 +347,7 @@ const spells = [
     type: "utility",
     tag: ("cleric","wizard"),
     description: {
-      desc: "You create an invisible, magical eye within range that hovers in the air for the duration. <br />You mentally receive visual information from the eye, which has normal vision and darkvision out to 30 feet. The eye can look in every direction.",
-        "As an action, you can move the eye up to 30 feet in any direction. There is no limit to how far away from you the eye can move, but it can't enter another plane of existence. A solid barrier blocks the eye's movement, but the eye can pass through an opening as small as 1 inch in diameter.",
+      desc: "You create an invisible, magical eye within range that hovers in the air for the duration. <br />You mentally receive visual information from the eye, which has normal vision and darkvision out to 30 feet. The eye can look in every direction.<br /><br />As an action, you can move the eye up to 30 feet in any direction. There is no limit to how far away from you the eye can move, but it can't enter another plane of existence. A solid barrier blocks the eye's movement, but the eye can pass through an opening as small as 1 inch in diameter.",
       higherLevel: null,
       school: "divination",
       range: "30 feet",
@@ -4129,3 +4128,5 @@ const spells = [
     }
   }
 ]
+
+export default spells
