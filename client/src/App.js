35f89d13a4cd4b8945dd2bc8943043d1706c66aa
Garth Hammonds <72636991+Hammonds526@users.noreply.github.com>
2021-04-19 20:43:41 -0700

    changed API.getUser var

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   useEffect(() => {
     // TO DO: REPLACE THIS HASH WITH AUTHENTICATED USER
-    API.getUser(process.env.REACT_APP_USER_ID).then((res) => {
+    API.getUser("085189151981561189651985" || process.env.REACT_APP_USER_ID).then((res) => {
       console.log(res.data);
       setmyCharacters(res.data.user.characters);
       console.log(myCharacters);
