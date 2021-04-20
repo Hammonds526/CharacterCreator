975c3cc3b3d2df095747080f778ac18e00e3383f
Relten98 <diezhandinoty@gmail.com>
2021-04-20 15:46:39 -0700

    CSS updates, and Char Sheet update.

--- client/src/components/ButtonClose/index.js
+++ client/src/components/ButtonClose/index.js
@@ -5,14 +5,22 @@ import Xbtn from "../../images/xbtn.png";
 
 // Our X-btn function, should close the main page when clicked.
 function ButtonClose() {
-    
 
-
-    return(
-
-        <button>
-
-        </button>
+    return (
+        <div className="position-relative" onClick={props.selectButtonOnClick}>
+            <Xbtn
+                arrowStyle={{
+                    width: "18px",
+                    height: "auto",
+                    left: "68px",
+                    top: "6px",
+                    zIndex: "1",
+                }}
+            />
+            <div className="position-relative button-image-background">
+                <p className="font-weight-bold select-button__text">{props.text}</p>
+            </div>
+        </div>
     )
 };
 
--- client/src/components/CharacterSheet/index.js
+++ client/src/components/CharacterSheet/index.js
@@ -7,6 +7,13 @@ import { Route, useRouteMatch } from "react-router-dom";
 // CSS
 // import "./style.css";
 
+// Icons
+import Fighter from "../../images/icons/fighter.png";
+import Wizard from "../../images/icons/wizard.png";
+import Ranger from "../../images/icons/ranger.png";
+import Rogue from "../../images/icons/rogue.png";
+
+
 // Components.
 
 // Character Sheet Function.
@@ -14,24 +21,25 @@ ComponentDidMount = () => {
     this.GetCharacters();
 };
 
-GetCharacters= () => {
+GetCharacters = () => {
     axios.get('/api')
-    .then((response) => {
-        let Generatedcharacter = response.data;
-        this.setState()
-    console.log('Character data has been recieved!')
-    })
-    .catch(() => {
-        console.log('Error retriving character data')
-    })
+        .then((response) => {
+            let Generatedcharacter = response.data;
+            this.setState()
+            console.log('Character data has been recieved!')
+        })
+        .catch(() => {
+            console.log('Error retriving character data')
+        })
 }
 
 function CharacterSheet(props) {
+let savedCharacter = 
 
     let { path } = useRouteMatch();
-    const [savedCharacter, setNewSheet] = useState({
+    const [savedCharacter, printCharacter] = useState({
 
-        name: "",
+        name: "Cheese",
         level: 1,
         race: "dwarf",
         class: "fighter",
@@ -43,9 +51,25 @@ function CharacterSheet(props) {
 
     });
 
+    // Sets icon for sheet.
+    // if (item.class = "fighter") {
+    //     let Classicon = Fighter;
+    // } else if (item.class = "wizard") {
+    //     let Classicon = Wizard;
+    // } else if (item.class = "ranger") {
+    //     let Classicon = Ranger;
+    // } else (item.class = "rogue") {
+    //     let Classicon = Rogue;
+    // };
+
+    // The information that is going to be displayed in react.
     return (
         <div className="modal-content-box">
-
+            <h1>Character Sheet</h1>
+            <br />
+            <h2>{item.name}</h2>
+            <h2 id="lvlbox">{item.level}</h2>
+            <h2>{item.class}</h2>
         </div>
     );
 }
