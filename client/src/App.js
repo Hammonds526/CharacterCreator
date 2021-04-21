d65e7cd22ed84e5dac94e0fe80435027de537d0a
C.T <ct.bell@yahoo.com>
2021-04-20 21:02:04 -0400

    Adding 'if' statement to new character api get requst to keep the page from breaking when the db isn't working

--- client/src/App.js
+++ client/src/App.js
@@ -14,10 +14,15 @@ function App() {
 
   useEffect(() => {
     // TO DO: REPLACE THIS HASH WITH AUTHENTICATED USER
-    API.getUser("085189151981561189651985" || process.env.REACT_APP_USER_ID).then((res) => {
-      console.log(res.data);
-      setmyCharacters(res.data.user.characters);
-      console.log(myCharacters);
+    API.getUser(process.env.REACT_APP_USER_ID).then((res) => {
+      // console.log(res.data.user.characters);
+      console.log("res.data ", res.data);
+      if (!res.data === null) {
+        
+        setmyCharacters(res.data.user.characters);
+       
+      } 
+      // console.log(myCharacters);
     });
   }, []);
 
