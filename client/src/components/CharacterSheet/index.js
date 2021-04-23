e308b09fb2f6cd3316f533ea93dad2e10b851178
Relten98 <diezhandinoty@gmail.com>
2021-04-22 21:44:26 -0700

    UPDATED CHARACTER SHEET

--- client/src/components/CharacterSheet/index.js
+++ client/src/components/CharacterSheet/index.js
@@ -6,19 +6,10 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 
+
 // Main function. 
-
- //  Using the temporarily created character stored in the newcharacter array, and not the ones in the DB. Helps with offline
+//  Using the temporarily created character stored in the newcharacter array, and not the ones in the DB. Helps with offline
 function CharacterSheet({ newCharacter }) {
-
-    // let Characterdesc = Legend;
-    // let Legend = "";
-    // if (newCharacter.class == "Commoner") {
-    //     let Legend = "I sense great power in this one..."
-    
-    // } else {
-    //     let Legend = newCharacter.subclass;
-    // };
 
     console.log("newCharacter from Sheet ", newCharacter);
     // The information that is going to be displayed in react.
@@ -26,14 +17,18 @@ function CharacterSheet({ newCharacter }) {
         <div className="modal-content-box">
             <h1>Character Sheet</h1>
             <br />
-            <h2>{newCharacter.name}</h2>
-            <h2 id="lvlbox">{newCharacter.level}</h2>
-            <h2>{newCharacter.class}</h2>
+            <div>
+                <h2>{newCharacter.name}</h2>
+                <br />
+                <h2 id="lvlbox">Level : {newCharacter.level}</h2>
+                <h2>{newCharacter.class}</h2>
+            </div>
             <br />
-            {/* <h2>{Legend}</h2> */}
-            <br />
-            <p>{newCharacter.spells}</p>
-            <p>{newCharacter.feats}</p>
+            <div>
+                <p>{newCharacter.spells}</p>
+                <br />
+                <p>{newCharacter.feats}</p>
+            </div>
             <br />
         </div>
     );
