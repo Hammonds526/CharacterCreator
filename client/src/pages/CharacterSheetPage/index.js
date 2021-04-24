6ffa2abd590046f76149b91761b25e646ffdf50c
Relten98 <diezhandinoty@gmail.com>
2021-04-24 15:11:41 -0700

    Update index.js
    Starting to implement proper spell listing, will have to continue this later this evening.

--- client/src/pages/CharacterSheetPage/index.js
+++ client/src/pages/CharacterSheetPage/index.js
@@ -9,7 +9,9 @@ import WoodBeamX from "../../components/WoodBeamX";
 import WoodBeamY from "../../components/WoodBeamY";
 
 // Stone Border
-
+import Corner2 from "../../components/Corner2"
+import StonebeamX from "../../components/StonebeamX";
+import StonebeamY from "../../components/StonebeamY";
 // Main function. 
 //  Using the temporarily created character stored in the newcharacter array, and not the ones in the DB. Helps with offline
 function CharacterSheetPage({ newCharacter }) {
@@ -53,7 +55,7 @@ function CharacterSheetPage({ newCharacter }) {
                             <br />
                             <div className="col-12">
                                 <div className="col-7">
-                                <h2 id="character-name">{newCharacter.name}</h2>
+                                    <h2 id="character-name">{newCharacter.name}</h2>
                                     <br />
                                     <h2>Race: {newCharacter.race.charAt(0).toUpperCase() + newCharacter.race.slice(1)}{" "}</h2>
                                     <h2>Class: {newCharacter.class.charAt(0).toUpperCase() + newCharacter.class.slice(1)}</h2>
@@ -62,14 +64,55 @@ function CharacterSheetPage({ newCharacter }) {
                                     <br />
                                     <div>
                                         <h2>Level :</h2>
-                                    <h2 id="lvlbox">{newCharacter.level}</h2>
+                                        <h2 id="lvlbox">{newCharacter.level}</h2>
                                     </div>
                                     <br />
                                 </div>
                                 <div className="col-5">
+                                    {/* <StonebeamX beamStyle={{ top: "-2px" }} />
+
+                                    <StonebeamY beamStyle={{ right: "10px", top: "9px" }} />
+                                    <StonebeamY beamStyle={{ left: "10px", top: "9px" }} />
+
+                                    <Corner2
+                                        cornerStyle={{
+                                            width: "80px",
+                                            height: "auto",
+                                            left: "-5px",
+                                            top: "-2px",
+                                        }}
+                                    />
+                                    <Corner2
+                                        cornerStyle={{
+                                            width: "80px",
+                                            height: "auto",
+                                            right: "-4px",
+                                            top: "-2px",
+                                            transform: "rotate(90deg)",
+                                        }}
+                                    /> */}
                                     <p>{newCharacter.spells}</p>
                                     <br />
                                     <p>{newCharacter.feats}</p>
+                                    {/* <StonebeamX beamStyle={{ bottom: "-3px" }} />
+                        <Corner2
+                            cornerStyle={{
+                                width: "80px",
+                                height: "auto",
+                                right: "-4px",
+                                bottom: "-4px",
+                                transform: "rotate(180deg)",
+                            }}
+                        />
+                        <Corner2
+                            cornerStyle={{
+                                width: "80px",
+                                height: "auto",
+                                left: "-4px",
+                                bottom: "-4px",
+                                transform: "rotate(270deg)",
+                            }}
+                        /> */}
                                 </div>
                             </div>
                         </div>
