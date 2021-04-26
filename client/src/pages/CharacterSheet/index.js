4de5044b0f124624162dd2ad40bc60a293d721c2
Relten98 <diezhandinoty@gmail.com>
2021-04-26 08:37:58 -0700

    WOO HOO

--- client/src/pages/CharacterSheet/index.js
+++ client/src/pages/CharacterSheet/index.js
@@ -1,38 +1,26 @@
-import React from "react"
+// Important React Imports.
+import React from "react";
+import { useParams } from "react-router-dom";
 
 // CSS
 import "./style.css";
 
-// Wood border
+// Components
 import Corner from "../../components/Corner";
 import WoodBeamX from "../../components/WoodBeamX";
 import WoodBeamY from "../../components/WoodBeamY";
-
-
-// Close btn
 import Xbtn from "../../components/ButtonClose";
 
-// Main function. 
+// Main function.
 //  Using the temporarily created character stored in the newcharacter array, and not the ones in the DB. Helps with offline
-function CharacterSheetPage({ myCharacters }) {
-    // let { id } = useParams();
-    // The information that is going to be displayed in react.
-    return (
-        <div className="modal-content-box">
-            <Xbtn
-                xBtnStyle={{
-                    width: "30px",
-                    height: "auto",
-                    right: "9px",
-                    top: "6px",
-                    zIndex: "2",
-                }}
-                xBtnUrl="/"
-            />
-            < div className="modal-content-box">
-                <br />
-                <div className="col">
-                    <WoodBeamX beamStyle={{ top: "-2px" }} />
+function CharacterSheet({ myCharacters }) {
+  let { id } = useParams();
+  //   console.log("newCharacter from Sheet ", newCharacter);
+  // The information that is going to be displayed in react.
+
+  return (
+    <div className="modal-content-box">
+                          <WoodBeamX beamStyle={{ top: "-2px" }} />
 
                     <WoodBeamY beamStyle={{ right: "10px", top: "9px" }} />
                     <WoodBeamY beamStyle={{ left: "10px", top: "9px" }} />
@@ -55,32 +43,37 @@ function CharacterSheetPage({ myCharacters }) {
                         }}
                     />
                     {/* This this button below is supposed to close the creator */}
-                    <div id="divbox">
-                        <br />
-                        <h1>My Character</h1>
-                        <br />
-                        <div className="col-12">
-                            <div className="col-7">
-                                <h2 id="character-name">{myCharacters[id].name ? myCharacters[id].name : "No-Name Baggins"}</h2>
-                                <br />
-                                <h2>{myCharacters[id].class}</h2>
-                                <br />
-                                <h2>{myCharacters[id].subclass}</h2>
-                                <br />
-                                <div>
-                                    <h2 id="lvlbox">Level : {myCharacters[id].level}</h2>
-                                </div>
-                                {/* < AvatarContainer /> */}
-                                <br />
-                            </div>
-                            <div className="col-5">
-                                <p>{myCharacters[id].spells}</p>
-                                <br />
-                                <p>{myCharacters[id].feats}</p>
-                            </div>
-                        </div>
-                    </div>
-                    {/* Nothing below here */}
+      <Xbtn
+        xBtnStyle={{
+          width: "30px",
+          height: "auto",
+          right: "9px",
+          top: "6px",
+          zIndex: "2",
+        }}
+        xBtnUrl="/"
+      />
+      <br />
+      <h1>My Character</h1>
+      <br />
+      <div className="container">
+      <div className="col-6">
+        <h2>
+          {myCharacters[id].name ? myCharacters[id].name : "No-Name Baggins"}
+        </h2>
+        <h2>{myCharacters[id].class}</h2>
+        <h2>{myCharacters[id].subclass}</h2>
+        <br />
+        <h2 id="lvlbox">Level : {myCharacters[id].level}</h2>
+      </div>
+      <br />
+      <div className="col-5">
+        <p>{myCharacters[id].spells}</p>
+        <br />
+        <p>{myCharacters[id].feats}</p>
+      </div>
+      </div>
+                          {/* Nothing below here */}
                     <WoodBeamX beamStyle={{ bottom: "-3px" }} />
                     <Corner
                         cornerStyle={{
@@ -100,10 +93,9 @@ function CharacterSheetPage({ myCharacters }) {
                             transform: "rotate(270deg)",
                         }}
                     />
-                </div>
-            </div>
-        </div>
-    );
+    </div>
+  );
 }
 
-export default CharacterSheetPage;
+// Good god, this makes me want to die.
+export default CharacterSheet;
