b332b6de9207f9eb0598ede0e3aa275c4d2b0366
Relten98 <diezhandinoty@gmail.com>
2021-04-26 09:08:34 -0700

    On the right track, wrapping this mess up

--- client/src/pages/CharacterSheet/index.js
+++ client/src/pages/CharacterSheet/index.js
@@ -14,87 +14,101 @@ import Xbtn from "../../components/ButtonClose";
 // Main function.
 //  Using the temporarily created character stored in the newcharacter array, and not the ones in the DB. Helps with offline
 function CharacterSheet({ myCharacters }) {
-  let { id } = useParams();
-  //   console.log("newCharacter from Sheet ", newCharacter);
-  // The information that is going to be displayed in react.
+    let { id } = useParams();
+    //   console.log("newCharacter from Sheet ", newCharacter);
+    // The information that is going to be displayed in react.
 
-  return (
-    <div className="modal-content-box">
-                          <WoodBeamX beamStyle={{ top: "-2px" }} />
+    return (
+        <div className="modal-content-box">
+            <div id="divbox" className="container">
+                <WoodBeamX beamStyle={{ top: "-2px" }} />
 
-                    <WoodBeamY beamStyle={{ right: "10px", top: "9px" }} />
-                    <WoodBeamY beamStyle={{ left: "10px", top: "9px" }} />
+                <WoodBeamY beamStyle={{ right: "29px", top: "9px" }} />
+                <WoodBeamY beamStyle={{ left: "29px", top: "9px" }} />
 
-                    <Corner
-                        cornerStyle={{
-                            width: "80px",
-                            height: "auto",
-                            left: "-5px",
-                            top: "-2px",
-                        }}
-                    />
-                    <Corner
-                        cornerStyle={{
-                            width: "80px",
-                            height: "auto",
-                            right: "-4px",
-                            top: "-2px",
-                            transform: "rotate(90deg)",
-                        }}
-                    />
-                    {/* This this button below is supposed to close the creator */}
-      <Xbtn
-        xBtnStyle={{
-          width: "30px",
-          height: "auto",
-          right: "9px",
-          top: "6px",
-          zIndex: "2",
-        }}
-        xBtnUrl="/"
-      />
-      <br />
-      <h1>My Character</h1>
-      <br />
-      <div className="container">
-      <div className="col-6">
-        <h2>
-          {myCharacters[id].name ? myCharacters[id].name : "No-Name Baggins"}
-        </h2>
-        <h2>{myCharacters[id].class}</h2>
-        <h2>{myCharacters[id].subclass}</h2>
-        <br />
-        <h2 id="lvlbox">Level : {myCharacters[id].level}</h2>
-      </div>
-      <br />
-      <div className="col-5">
-        <p>{myCharacters[id].spells}</p>
-        <br />
-        <p>{myCharacters[id].feats}</p>
-      </div>
-      </div>
-                          {/* Nothing below here */}
-                    <WoodBeamX beamStyle={{ bottom: "-3px" }} />
-                    <Corner
-                        cornerStyle={{
-                            width: "80px",
-                            height: "auto",
-                            right: "-4px",
-                            bottom: "-4px",
-                            transform: "rotate(180deg)",
-                        }}
-                    />
-                    <Corner
-                        cornerStyle={{
-                            width: "80px",
-                            height: "auto",
-                            left: "-4px",
-                            bottom: "-4px",
-                            transform: "rotate(270deg)",
-                        }}
-                    />
-    </div>
-  );
+                <Corner
+                    cornerStyle={{
+                        width: "80px",
+                        height: "auto",
+                        left: "-5px",
+                        top: "-2px",
+                    }}
+                />
+                <Corner
+                    cornerStyle={{
+                        width: "80px",
+                        height: "auto",
+                        right: "-4px",
+                        top: "-2px",
+                        transform: "rotate(90deg)",
+                    }}
+                />
+                {/* This this button below is supposed to close the creator */}
+                <Xbtn
+                    xBtnStyle={{
+                        width: "30px",
+                        height: "auto",
+                        right: "2px",
+                        top: "2px",
+                        zIndex: "2",
+                    }}
+                    xBtnUrl="/"
+                />
+                <div id="divbox">
+                    <br />
+                    <div className="d-flex justify-content-center">
+                        <h1>My Character</h1>
+                    </div>
+                    <br />
+                    <div className="row">
+                        <div className="col-7">
+                            <div>
+                                <h2>{myCharacters[id].name ? myCharacters[id].name : "No-Name Baggins"}</h2>
+                            </div>
+                            <br />
+                            <div>
+                                <h2>{myCharacters[id].class}</h2>
+                                <h2>{myCharacters[id].subclass}</h2>
+                                <br />
+                                <h2 id="lvlbox">Level : {myCharacters[id].level}</h2>
+                            </div>
+                        </div>
+                        <br />
+                        <div id="darkback" className="col-5">
+                            <div id="listman">
+                                <h2 id="listnames">Spells:</h2>
+                                <p>{myCharacters[id].spells}</p>
+                                <br />
+                                <h2 id="listnames">Feats:</h2>
+                                <p>{myCharacters[id].feats}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                </div>
+                {/* Nothing below here */}
+                <WoodBeamX beamStyle={{ bottom: "-3px" }} />
+                <Corner
+                    cornerStyle={{
+                        width: "80px",
+                        height: "auto",
+                        right: "-4px",
+                        bottom: "-4px",
+                        transform: "rotate(180deg)",
+                    }}
+                />
+                <Corner
+                    cornerStyle={{
+                        width: "80px",
+                        height: "auto",
+                        left: "-8px",
+                        bottom: "-4px",
+                        transform: "rotate(270deg)",
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
 
 // Good god, this makes me want to die.
