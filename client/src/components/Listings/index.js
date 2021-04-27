5302aa9bfbeb2c62fb98dbb3449e60151b6b6134
Curtis H <sitrucrettah1@gmail.com>
2021-04-26 21:53:26 -0700

    Add Map

--- client/src/components/Listings/index.js
+++ client/src/components/Listings/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 function Listings(props) {
   // console.log(props.items);
+  // const [firstItem, setFirstItem] = useState([]);
+
   return (
     <Row>
       <Col xs={10}>
