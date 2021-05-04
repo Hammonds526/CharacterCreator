1d9e4204eca243ebbcf7c1eb9cd4d00243e988bc
Relten98 <diezhandinoty@gmail.com>
2021-05-03 23:08:43 -0700

    updated deletion btn to use the proper Icon

---
+++ client/src/components/Deletebtn/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./style.css";
+
+// Btn image
+import Trashbtn from "../../images/ui/delete.png";
+
+// Our X-btn function, should close the main page when clicked.
+function Deletebtn(props) {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push(props.xBtnUrl);
+  }
+
+  return (
+    <div className="position-relative">
+      <img
+        src={Trashbtn}
+        style={props.DeletebtnStyle}
+        id="Deletebtn"
+        className="position-absolute"
+        onClick={props.DeletebtnFunc ? props.DeletebtnFunc : handleClick}
+      />
+    </div>
+  );
+}
+
+export default Deletebtn;
